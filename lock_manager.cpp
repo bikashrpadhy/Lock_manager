8476ec739c24525048207da6296ef29766fc8d3d
Bikash Ranjan Padhy <61779045+bikashrpadhy@users.noreply.github.com>
2024-03-09 15:59:06 +0530

    Update lock_manager.cpp
     upgrade, downgrade

--- lock_manager.cpp
+++ lock_manager.cpp
@@ -1,20 +1,20 @@
-/* MTCS-203(DBMS) Assignment A2 -- Lock Manager
+/* MTCS-203(DBMS) -- Lock Manager
  * 
- * Goal : Build a lock manager. 
- * The lock manager should support the following capabilities:
+ * Goal : Build a lock manager
+ * The lock manager supports the following capabilities:
  * 1. Lock a resource in either shared or exclusive mode. 
  * 2. Unlock a resource held by a transaction. 
- * A resource will be identified by a 10 character string.  
+ * 3. Supports for lock upgrade
+ * 4. Supports for lock downgrade
  *
- * Submitted by- Bikash Ranjan Padhy,
- * Regd_no: 22554
+ * Built by- Bikash Ranjan Padhy,
  */
 
 #include <iostream>
 #include <unordered_map>
 #include <list>
 #include <iterator>
-#include<string>
+#include <string>
 
 using namespace std;
 
@@ -52,6 +52,12 @@ public:
     lock_status_ = st;
     return true;
   }
+  bool setLockType(uint8_t ty) //created for downgrade function
+  {
+    lock_type_ = ty;
+    return true;
+  }
+
   string enum_to_string(const int eenum)
   {
   switch (eenum)
@@ -100,9 +106,13 @@ enum lockStatus
   
 };
 
-bool lock(std::string resource_name, std::uint32_t txn_id, uint8_t lock_type);
+uint8_t lock(std::string resource_name, std::uint32_t txn_id, uint8_t lock_type);
 
 uint8_t unlock(std::string resource_name, std::uint32_t txn_id);
+
+uint8_t downgrade(std::string resource_name, std::uint32_t txn_id);
+
+uint8_t upgrade(std::string resource_name, std::uint32_t txn_id);
 
 unordered_map<std::string, list<lockable_resource *>*> lock_table;
 
@@ -128,7 +138,7 @@ int main()
   ret = lock("AAA", 4567, lockType::SHARED); 
   ret= lock("AAA", 4568, lockType::EXCLUSIVE);
   ret= lock("AAA", 4569, lockType::EXCLUSIVE);
-  ret= lock("BBB", 4570, lockType::SHARED);
+  ret= lock("BBB", 4570, lockType::SHARED); //downgrade this later
   ret= lock("AAA", 4571, lockType::SHARED);
   ret= lock("BBB", 4572, lockType::EXCLUSIVE);
   cout<<"\n\n  *******************************All Locks acquired"<<endl;
@@ -137,11 +147,14 @@ int main()
   try
   {
     unlock("AAA", 4567);
+    downgrade("BBB", 4570);
+    upgrade("BBB", 4570);
   }
   catch(std::invalid_argument& e)
   {
     cerr << e.what() << endl;
   }
+
 
   displaylock_table(lock_table);
   return 0;
@@ -154,7 +167,7 @@ int main()
 /// @param txn_id transaction id of the transaction
 /// @param lock_type the kind of lock(Shared/ Exclusive) being requested 
 /// @return True if lock was acquired 
-bool lock(std::string resource_name, std::uint32_t txn_id, uint8_t lock_type)
+uint8_t lock(std::string resource_name, std::uint32_t txn_id, uint8_t lock_type)
 {
   uint8_t retval = lockStatus::WAITING;
 
@@ -313,4 +326,122 @@ uint8_t unlock(std::string resource_name, std::uint32_t txn_id)
   }
   return retval;
 
+}
+
+
+uint8_t downgrade(std::string resource_name, std::uint32_t txn_id)
+{
+  //We need to check if the txn_id holds an EXCLUSIVE lock on the resource_name
+  // If it doesnt, then we cant downgrade it, else we can
+  // 1. we need to unlock the resource_name with txn_id
+  // 2. Acquire a SHARED lock on resource_name with the same txn_id
+
+  uint8_t ret = lockStatus::WAITING;
+  if (lock_table.find(resource_name) == lock_table.end())
+  {
+    // Couldnt find the resource to be unlocked in the lock_table
+    // Invalid unlock request, throw an exception (error message)
+    throw std::invalid_argument("Cant unlock something that's not locked");
+  }
+  else
+  {
+    list<lockable_resource*> *lst = lock_table[resource_name];
+    bool not_granted;
+
+    auto lr = lst->begin();
+    for( ; lr !=lst->end(); ++lr)
+    {
+      if((*lr)->getTxnId()== txn_id ) 
+      {
+        if((*lr)->getStatus()== lockStatus::GRANTED && (*lr)->getLockType()==lockType::EXCLUSIVE)
+        {
+          cout<<"\n$$$$$$$$$$$$   Found this transaction's lockable resource to be **DOWNGRADED**: "<<endl;
+          (*lr)->displayLockableResource(resource_name);
+
+          (*lr)->setLockType(lockType::SHARED);
+          not_granted= false;
+          break;
+        }
+        else
+        {
+          // Found the lockable resource of this txn_id, but its still in WAITING lockStatus
+          not_granted= true;
+        }
+      }
+      
+    }
+    if(not_granted)
+    {
+      cout<<"Exclusive Lock wasn't GRANTED to txn_id: "<< txn_id <<"."<<endl; 
+      cout<<"So it must acquire an Exclusive lock on "<< resource_name<< " first."<<endl;
+      return ret;
+    }
+
+    bool shared_flag=false;
+    for(lr=lst->begin() ; lr!= lst->end(); ++lr)
+    {
+      // promote the waiting transaction(s) waiting in the queue(list)
+      // yeah, especaially the ones that came before the unlocked transaction
+      if((*lr)->getStatus()== lockStatus::WAITING)
+      {
+        if((*lr)->getLockType()== lockType::SHARED)
+        {
+          //promote all SHARED
+          (*lr)->setLockStatus(lockStatus::GRANTED);
+          ret= lockStatus::GRANTED; //unlock has been granted
+          shared_flag= true;        
+          continue;
+        }
+      }      
+    }
+  }  
+  return ret;
+}
+
+
+uint8_t upgrade(std::string resource_name, std::uint32_t txn_id)
+{
+  // We may upgrade Shared locks to Exclusive locks.
+  // A Shared lock cant be upgraded to an Exclusive lock if it is followed by a Shared/Exclusive lock thats granted.
+  uint8_t ret = lockStatus::WAITING;
+  if (lock_table.find(resource_name) == lock_table.end())
+  {
+    // Couldnt find the resource to be unlocked in the lock_table
+    // Invalid unlock request, throw an exception (error message)
+    throw std::invalid_argument("Cant upgrade a lock that's not acquired.");
+  }
+  else
+  {
+    list<lockable_resource*> *lst = lock_table[resource_name];
+    std::list<lockable_resource*>::iterator upgradelr;
+
+    auto lr = lst->begin();
+    for( ; lr !=lst->end(); ++lr)
+    {
+      if((*lr)->getTxnId()== txn_id ) 
+      {
+        if((*lr)->getStatus()== lockStatus::GRANTED && (*lr)->getLockType()==lockType::SHARED)
+        {
+          cout<<"\n$$$$$$$$$$$$   Found this transaction's lockable resource to be **UPGRADED**: "<<endl;
+          (*lr)->displayLockableResource(resource_name);
+          upgradelr= lr; //upgradelr now points at the transaction that may be upgraded 
+          break;
+        }
+      }
+    }
+    // from that index of lr, search for Granted locks in the same list;
+    // Even if one Granted lock is present, We cant upgrade this lock
+    for(++lr ; lr!= lst->end(); ++lr)
+    {
+      if((*lr)->getStatus()== lockStatus::GRANTED )
+        {
+          cout<<"Upgrade request cancelled as other transactions are holding locks on "<< resource_name <<endl;
+          return ret;
+        }
+    }
+    // We are now clear to upgrade the upgradelr transaction 
+    (*upgradelr)->setLockType(lockType::EXCLUSIVE);
+    ret = lockStatus::GRANTED;
+    return ret; 
+  }
 }
